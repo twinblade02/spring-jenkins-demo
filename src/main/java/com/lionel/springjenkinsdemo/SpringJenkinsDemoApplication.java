81c7e27e9d93999fc69993dd8f8210652d690f03
Lionel D'silva <maveiyk789@gmail.com>
2020-01-12 15:49:43 -0500

    first commmit

---
+++ src/main/java/com/lionel/springjenkinsdemo/SpringJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lionel.springjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lionel/springjenkinsdemo/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionel.springjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
